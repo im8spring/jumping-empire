79a9308ee9f959ca1155b572d01b2dcfdf97deb3
im8spring <168703188+im8spring@users.noreply.github.com>
2024-06-15 14:30:15 +0900

    test branch

--- sketch.js
+++ sketch.js
@@ -5,72 +5,80 @@ let gameOverFlag = false;
 
 let dinoSprites = [];
 let currentSpriteIndex = 0;
-let spriteUpdateInterval = 5; // 이미지 업데이트 간격 설정
+let spriteUpdateInterval = 5;
 
-let jumpImage; // 점프 이미지를 저장할 변수
+let jumpImage;
+
+const GAME_WIDTH = 1200;
+const GAME_HEIGHT = 300;
+const GAME_X_OFFSET = 50;
+const GAME_Y_OFFSET = 200;
 
 function preload() {
   for (let i = 0; i <= 6; i++) {
     dinoSprites.push(loadImage('assets/image_' + i + '.png'));
   }
-  jumpImage = loadImage('assets/jump.png'); // 점프 이미지를 미리 로드
+  jumpImage = loadImage('assets/jump.png');
 }
 
 function setup() {
-  createCanvas(1200, 300); 
+  createCanvas(1300, 1000); // 캔버스를 더 크게 만듦
   dino = new Dino();
   obstacles.push(new Obstacle());
 }
 
 function draw() {
-  background(102,153,255);
-
+  background(000,051,000);
+  
+  // 게임 영역을 설정
+  push();
+  translate(GAME_X_OFFSET, GAME_Y_OFFSET);
+  stroke(255);
+  noFill();
+  fill(051,000,000)
+  rect(0, 0, GAME_WIDTH, GAME_HEIGHT); // 게임 영역을 나타내는 사각형
+  
   if (!gameOverFlag) {
-    // Display score
-    textSize(40); // 글꼴 크기를 크게 변경
-    text('Score: ' + score, 20, 50); // 위치를 조금 이동
-    
-    // Update and display the dino
+    textSize(40);
+    fill(255);
+    text('Score: ' + score, 20, 50);
+
     dino.update();
     dino.show();
-    
-    // Create new obstacles
+
     if (frameCount % 60 === 0) {
       obstacles.push(new Obstacle());
     }
-    
-    // Update and display obstacles
+
     for (let i = obstacles.length - 1; i >= 0; i--) {
       obstacles[i].update();
       obstacles[i].show();
-      
-      // Check for collision with dino
+
       if (obstacles[i].hits(dino)) {
         gameOver();
       }
-      
-      // Remove offscreen obstacles
+
       if (obstacles[i].offscreen()) {
         obstacles.splice(i, 1);
         score++;
       }
     }
-    
-    // Update dino sprite animation
+
     if (frameCount % spriteUpdateInterval === 0) {
       currentSpriteIndex = (currentSpriteIndex + 1) % dinoSprites.length;
     }
   } else {
-    // Display game over message
-    textSize(64); // 글꼴 크기를 크게 변경
+    textSize(64);
     textAlign(CENTER, CENTER);
     fill(255, 0, 0);
-    text('Game Over', width / 2, height / 2);
-    textSize(40); // 글꼴 크기를 크게 변경
-    text('Press Space to Restart', width / 2, height / 2 + 50); // 위치를 조금 이동
+    text('Game Over', GAME_WIDTH / 2, GAME_HEIGHT / 2);
+    textSize(40);
+    text('Press Space to Restart', GAME_WIDTH / 2, GAME_HEIGHT / 2 + 50);
   }
+
+  pop();
 }
-  
+
 function keyPressed() {
   if (key === ' ') {
     if (gameOverFlag) {
@@ -98,61 +106,61 @@ function resetGame() {
 class Dino {
   constructor() {
     this.x = 100;
-    this.y = height - 100; // 조금 위로 이동
+    this.y = GAME_HEIGHT - 100;
     this.velocityY = 0;
-    this.gravity = 2; // 중력을 강화
+    this.gravity = 2;
     this.isJumping = false;
   }
-  
+
   jump() {
     if (!this.isJumping) {
-      this.velocityY = -35; // 점프 강도를 강화
+      this.velocityY = -35;
       this.isJumping = true;
     }
   }
-  
+
   update() {
     this.velocityY += this.gravity;
     this.y += this.velocityY;
-    
-    if (this.y >= height - 100) {
-      this.y = height - 100;
+
+    if (this.y >= GAME_HEIGHT - 100) {
+      this.y = GAME_HEIGHT - 100;
       this.velocityY = 0;
       this.isJumping = false;
     }
   }
-  
+
   show() {
-    if (this.isJumping) { // 점프 중인 경우 jumpImage 표시
-      image(jumpImage, this.x, this.y, 100, 100); // 크기를 2배로 확대
-    } else { // 그 외의 경우 dinoSprites 표시
-      image(dinoSprites[currentSpriteIndex], this.x, this.y, 100, 100); // 크기를 2배로 확대
+    if (this.isJumping) {
+      image(jumpImage, this.x, this.y, 100, 100);
+    } else {
+      image(dinoSprites[currentSpriteIndex], this.x, this.y, 100, 100);
     }
   }
 }
 
 class Obstacle {
   constructor() {
-    this.x = width;
-    this.y = height - 80; // 조금 위로 이동
-    this.width = 60; // 장애물의 너비를 크게 변경
-    this.height = 40; // 장애물의 높이를 크게 변경
-    this.speed = 9; // 속도를 빠르게 변경
-    this.img = loadImage('assets/craft.png'); // craft.png 이미지를 불러옴
+    this.x = GAME_WIDTH;
+    this.y = GAME_HEIGHT - 80;
+    this.width = 60;
+    this.height = 40;
+    this.speed = 9;
+    this.img = loadImage('assets/craft.png');
   }
-  
+
   update() {
     this.x -= this.speed;
   }
-  
+
   show() {
-    image(this.img, this.x, this.y, this.width, this.height); // 이미지로 장애물 표시
+    image(this.img, this.x, this.y, this.width, this.height);
   }
-  
+
   offscreen() {
     return this.x + this.width < 0;
   }
-  
+
   hits(dino) {
     return (
       dino.x + 100 > this.x &&
